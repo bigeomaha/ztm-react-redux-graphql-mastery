92b419185c9030e23dd7aed16ed4307f900be11d
Eric Lackore <webdeveloper79@yahoo.com>
2023-02-24 13:23:47 -0600

    Converting Component from class to functional

--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,18 +1,13 @@
-import {Component} from 'react';
+
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-    render() {
-        const { onSearchChangeHandler, placeholder,class_name } = this.props;
-        return (
-            <input
-                className={`search-box ${class_name}`}
-                type="search"
-                placeholder={placeholder}
-                onChange={onSearchChangeHandler} />
-        )
-    }
-}
+const SearchBox = ({ onSearchChangeHandler, placeholder, class_name }) => (
+    <input
+        className={`search-box ${class_name}`}
+        type="search"
+        placeholder={placeholder}
+        onChange={onSearchChangeHandler} />
+)
 
 export default SearchBox;
 
