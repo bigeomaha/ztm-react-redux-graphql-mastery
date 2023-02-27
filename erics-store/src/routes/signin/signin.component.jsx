5c9c0fd05d5ff0ade2fea431295cec88fc86260f
Eric Lackore <webdeveloper79@yahoo.com>
2023-02-27 16:48:48 -0600

    Removed Google Redirection method

--- erics-store/src/routes/signin/signin.component.jsx
+++ erics-store/src/routes/signin/signin.component.jsx
@@ -1,20 +1,9 @@
 import { async } from "@firebase/util";
 import { getRedirectResult } from "firebase/auth";
-import { useEffect } from "react";
-import { auth, signInWithGoogle, createUserDocumentFromAuth, signInWithGoogleRedirect } from "../../utils/firebase.utils";
+import { signInWithGoogle, createUserDocumentFromAuth, signInWithGoogleRedirect } from "../../utils/firebase.utils";
 
 
 const SignIn = () => {
-    useEffect( () => {
-        (async () => {
-            const response = await getRedirectResult(auth);
-
-            if (response) {
-                const user = await createUserDocumentFromAuth(response.user);
-            }
-        })()
-    }, []);
-
     const logInGoogleUser = async () => {
         const response = await signInWithGoogle();
         const user = await createUserDocumentFromAuth(response.user);
@@ -30,9 +19,6 @@ const SignIn = () => {
             <h1>Sign In</h1>
             <button onClick={logInGoogleUser}>
                 <span>Sign In with Google</span>
-            </button>
-            <button onClick={logInGoogleUserRedirect}>
-                <span>Sign In with Email</span>
             </button>
         </div>
     )
