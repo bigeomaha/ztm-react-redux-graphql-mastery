b1e35fcbc484637c467f660d0d2fea50fb135ed4
Eric Lackore <webdeveloper79@yahoo.com>
2023-02-27 15:24:06 -0600

    Updating with Logo

--- erics-store/src/components/navigation/navigation.component.jsx
+++ erics-store/src/components/navigation/navigation.component.jsx
@@ -1,14 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 import './navigation.styles.scss';
-
+import { ReactComponent as StoreLogo} from '../../assets/store-logo.svg';
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className='navigation'>
                 <Link to='/' className='logo-container'>
-                    <div>Logo</div>
+                    <div><StoreLogo/></div>
                 </Link>
                 <div className="nav-links-container">
                     <Link to='/' className='nav-link'>Home</Link>
