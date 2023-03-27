a89f8aa7bbcd9e951a88c1520efaab2b33badade
Eric Lackore <webdeveloper79@yahoo.com>
2023-03-27 15:35:16 -0500

    Link Categories on homepage

--- erics-store/src/components/category-item/category-item.component.jsx
+++ erics-store/src/components/category-item/category-item.component.jsx
@@ -1,15 +1,18 @@
 import './category-item.styles.scss';
+import { Link } from 'react-router-dom';
 
 const CategoryItem = ({ category }) => {
     const { id, title, imageUrl } = category;
     return(
-    <div className="directory-container" key={id}>
-        <div className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }} />
-        <div className="category-body-container">
-            <h2>{title.toUpperCase()}</h2>
-            <p>Shop Now</p>
+        <div className="directory-container" key={id}>
+            <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+                <div className="category-body-container">
+                <Link to={`/shop/${title}`} className='content'>
+                    <h2>{title.toUpperCase()}</h2>
+                    <p>Shop Now</p>
+                </Link>
+                </div>
         </div>
-    </div>)
+    )
 };
 export default CategoryItem;
