cf7fb097806716ae06f99eb2bfc8079ea120d3f4
Eric Lackore <webdeveloper79@yahoo.com>
2023-03-31 10:33:39 -0500

    Impimenting re-select caching for product catalog

--- erics-store/src/store/products/products.selector.js
+++ erics-store/src/store/products/products.selector.js
@@ -1,5 +1,17 @@
-export const selectProducts = state => state.products.product_catalog.reduce((acc, doc) => {
-    const { title, items } = doc.data();
-    acc[title.toLowerCase()] = items
-    return acc;
-}, {});
+import { createSelector } from 'reselect';
+
+const selectProductsReducer = state => state.products;
+
+export  const selectProductCatalog = createSelector(
+    [selectProductsReducer],
+    (products) => products.product_catalog
+);
+
+export const selectProducts = createSelector(
+    [selectProductCatalog],
+    (product_catalog) => product_catalog.reduce((acc, doc) => {
+        const { title, items } = doc.data();
+        acc[title.toLowerCase()] = items
+        return acc;
+    }, {})
+);
